0c42a01ef2beacae0a398ac4ec7f2c684412b52f
김수정 <145892697+Twoduel@users.noreply.github.com>
2024-05-15 17:39:25 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
 		long temp = 1;
 		
